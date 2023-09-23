7812005b95ebaa10a1803a325bb32faccbf736b0
DiegoX <diegomenendez17@gmail.com>
2023-09-23 17:42:47 -0600

    Terminando fase beta portafolio

--- src/components/Proyects.js
+++ src/components/Proyects.js
@@ -12,7 +12,7 @@ function Proyects() {
             <div className="top">
               <h3 className="proyect-title">Api Pedidos</h3>
               <p className="proyect-description">
-              Collaborative Project in the Development of an Orders API 
+              Collaborative <span>Backend</span> Project in the Development of an Orders API 
               with Spring Boot and JWT as authentication to the API requests,
                Mysql was used as a database and it was deployed in Apache, 
                testing with Mockito and Junit5.
@@ -27,16 +27,16 @@ function Proyects() {
             </div>
           </div>
           <div className="right">
-            {/*<a
+            {/* <a
               className="img-link"
-              href="https://gymate-site.netlify.app/"
+              href="https://gymate-site.netlify.app/"   Esta implementacion es para el despliegue de la API
               target="_blank"
-              rel="noreferrer"
-            >
+              rel="noreferrer" 
+            >*/}
             
               <div className="img-proyect-1"></div>
-            </a>
-            */}
+            
+            
 
             
             <div className="proyect-links">
@@ -52,7 +52,7 @@ function Proyects() {
           */}
           
               <a
-                href="https://github.com/NicolasFrancken/GYMATE"
+                href="https://github.com/ObedMP/ApiRestPedidos"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -64,24 +64,25 @@ function Proyects() {
 
         <div className="proyect-2 proyect-container">
           <div className="right">
-            <a
+            {/*<a
               className="img-link"
               href="https://nicavideo-webapp.netlify.app/"
               target="_blank"
               rel="noreferrer"
-            >
+        >*/}
               <div className="img-proyect-2"></div>
-            </a>
+            
             <div className="proyect-links">
+              {/*}
               <a
                 href="https://nicavideo-webapp.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="bx bx-sm bx-globe"></i>
-              </a>
+        </a> */}
               <a
-                href="https://github.com/NicolasFrancken/NicaVideo-Frontend"
+                href="https://github.com/ObedMP/TiendaEnLinea"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -91,21 +92,19 @@ function Proyects() {
           </div>
           <div className="left">
             <div className="top">
-              <h3 className="proyect-title">NicaVideo 🎥</h3>
+              <h3 className="proyect-title">Online Store 🛒</h3>
               <p className="proyect-description">
-                A video platform with user authentication, uploads, likes, etc.
-                Multiple pages, JWT auth, uploads and a full server (with a
-                PostgreSQL DB)
+              Online Store System managed with Shopping Carts and 
+              purchase processing module, using ASP.NET MVC
+              (with a SQL Server DB).
               </p>
             </div>
 
             <div className="bottom">
               <h3>Tech used</h3>
               <div className="proyect-skills">
-                <div className="skill-a"></div>
-                <div className="skill-g"></div>
-                <div className="skill-e"></div>
-                <div className="skill-f"></div>
+                <div className="skill-b"></div>
+                <div className="skill-k"></div>
               </div>
             </div>
           </div>
@@ -114,41 +113,41 @@ function Proyects() {
         <div className="proyect-3 proyect-container">
           <div className="left">
             <div className="top">
-              <h3 className="proyect-title">Songy &#127925;</h3>
+              <h3 className="proyect-title"> Library System 📖 </h3>
               <p className="proyect-description">
-                An interactive, with a loading interface, search app. Using
-                Deezer's API, I am able to request songs data. You can even
-                listen to a preview!
+              Library System developed in Java Spring boot with 
+              the MVC architecture, implementing the Angular frontend 
+              for managing views and forms. With Postgresql to DB.
               </p>
             </div>
             <div className="bottom">
               <h3>Tech used</h3>
               <div className="proyect-skills">
                 <div className="skill-a"></div>
-                <div className="skill-c"></div>
-                <div className="skill-g"></div>
+                <div className="skill-d"></div>
+                <div className="skill-spring"></div>
               </div>
             </div>
           </div>
           <div className="right">
-            <a
+            {/*<a
               className="img-link"
               href="https://songy-webapp.netlify.app/"
               target="_blank"
               rel="noreferrer"
-            >
+      >*/}
               <div className="img-proyect-3"></div>
-            </a>
+            
             <div className="proyect-links">
-              <a
+              {/*<a
                 href="https://songy-webapp.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="bx bx-sm bx-globe"></i>
-              </a>
+                </a>*/}
               <a
-                href="https://github.com/NicolasFrancken/Songy"
+                href="https://github.com/ObedMP/Sistema_Bibliotecario"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -160,24 +159,24 @@ function Proyects() {
 
         <div className="proyect-4 proyect-container">
           <div className="right">
-            <a
+           {/*} <a
               className="img-link"
               href="https://notecloud-webapp.netlify.app/"
               target="_blank"
               rel="noreferrer"
-            >
+              >*/}
               <div className="img-proyect-4"></div>
-            </a>
+            
             <div className="proyect-links">
-              <a
+              {/*<a
                 href="https://notecloud-webapp.netlify.app/"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="bx bx-sm bx-globe"></i>
-              </a>
+              </a>*/}
               <a
-                href="https://github.com/NicolasFrancken/NoteCloud"
+                href="https://github.com/ObedMP/ManageCustomer"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -187,11 +186,11 @@ function Proyects() {
           </div>
           <div className="left">
             <div className="top">
-              <h3 className="proyect-title">NoteCloud &#128221;</h3>
+              <h3 className="proyect-title">Customer Manage System</h3>
               <p className="proyect-description">
-                An interactive Notes App. With a Node + Express server, I
-                simulated a "cloud" experience. You can Create, Update and
-                Delete notes.
+               Backend Project Using Java EE, a small client control system was implemented
+               using servlets and jsp, also adding DAO data patterns, Mysql 
+               with JDBC was used for the data. Boostrap was used for the views.
               </p>
             </div>
 
@@ -199,10 +198,7 @@ function Proyects() {
               <h3>Tech used</h3>
               <div className="proyect-skills">
                 <div className="skill-a"></div>
-                <div className="skill-c"></div>
-                <div className="skill-e"></div>
-                <div className="skill-g"></div>
-                <div className="skill-h"></div>
+                
               </div>
             </div>
           </div>
